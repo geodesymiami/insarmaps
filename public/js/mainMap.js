2df032fb7209efd1abcba5abaf915306503e63fd
Zishi Wu <zishi@insarVMCSC431.insarVMCSC431.j5.internal.cloudapp.net>
2016-05-31 00:12:35 +0000

    fized graph color system

--- public/js/mainMap.js
+++ public/js/mainMap.js
@@ -92,9 +92,11 @@ function Map(loadJSONFunc) {
                     'circle-color': {
                         property: 'm',
                         stops: [
-                            [0, 'red'],
-                            [0.001, 'yellow'],
-                            [0.005, 'green']
+                            [-0.006, '#0000FF'], // blue
+                            [-0.003, '#00FFFF'], // cyan
+                            [0.0, '#01DF01'], // lime green
+                            [0.003, '#FFBF00'], // yellow orange
+                            [0.006, '#FF0000']   // red orange
                         ]
                     },
                     'circle-radius': {
