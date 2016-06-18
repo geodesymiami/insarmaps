183c5c3eab8c733823f950f8fda97e3f07b6be3d
stackTom <rtiky23@gmail.com>
2016-06-17 21:05:56 -0400

    num chunks

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -202,7 +202,6 @@ function getGEOJSON(area) {
     // make streets toggle button be only checked one
     $("#streets").prop("checked", true);
 
-    console.log("IT IS " + area.coords.num_chunks);
     for (var i = 1; i <= area.coords.num_chunks; i++) {
         var layer = { "id": "chunk_" + i, "description": "", "minzoom": 0, "maxzoom": 14, "fields": { "c": "Number", "m": "Number", "p": "Number" } };
         myMap.tileJSON.vector_layers.push(layer);
