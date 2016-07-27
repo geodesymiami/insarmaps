bd2749ad8a9b8d0e315e3d4f414a4c4d99e91f82
stackTom <rtiky23@gmail.com>
2016-07-27 17:23:41 -0400

    fixed postgres to php array to handle polygon

--- app/Http/Controllers/GeoJSONController.php
+++ app/Http/Controllers/GeoJSONController.php
@@ -33,7 +33,26 @@ class GeoJSONController extends Controller {
     $postgresStr = trim($pgArray, "{}");
     $elmts = explode(",", $postgresStr);
 
-    return $elmts;
+    $arrayToReturn = [];
+    $arrayLen = count($elmts);
+
+    for ($i = 0; $i < $arrayLen; $i++) {
+      $curString = $elmts[$i];      
+
+      if (strpos($curString, "POLYGON") !== false) {
+        $curString = substr($curString, 1);
+        $curString = $curString . " " . $elmts[$i + 1];
+        $curString = $curString . " " . $elmts[$i + 2];
+        $curString = $curString . " " . $elmts[$i + 3];
+        $curString = substr($curString, 0, strlen($curString) - 1);
+
+        $i += 3;
+      }
+
+      array_push($arrayToReturn, $curString);
+    }
+
+    return $arrayToReturn;
   }
 
   private function stringArrayToFloatArray($array) {    
