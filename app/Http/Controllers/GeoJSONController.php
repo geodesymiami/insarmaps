c9b82250d4b6bdcef17c59429aabef35c50f7c8a
stackTom <rtiky23@gmail.com>
2016-07-27 13:49:43 -0400

    fixed php to return array of strings vs a huge strong for attribute keys and values

--- app/Http/Controllers/GeoJSONController.php
+++ app/Http/Controllers/GeoJSONController.php
@@ -150,8 +150,8 @@ public function getAreas() {
       $currentArea["coords"]["longitude"] = $area->longitude;                
       $currentArea["num_chunks"] = $area->numchunks;
       $currentArea["country"] = $area->country;
-      $currentArea["attributekeys"] = $area->attributekeys;
-      $currentArea["attributevalues"] = $area->attributevalues;
+      $currentArea["attributekeys"] = $this->postgresToPHPArray($area->attributekeys);
+      $currentArea["attributevalues"] = $this->postgresToPHPArray($area->attributevalues);
       array_push($json["areas"], $currentArea);
     }
 
