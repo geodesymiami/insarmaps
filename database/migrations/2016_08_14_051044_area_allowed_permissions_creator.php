18a62caef3de235058fcc41d62794086ec109b07
vagrant <vagrant@homestead>
2016-08-14 05:16:05 +0000

    new migrations for permissions system

--- database/migrations/2016_08_14_051044_area_allowed_permissions_creator.php
+++ database/migrations/2016_08_14_051044_area_allowed_permissions_creator.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PermissionsTableCreator extends Migration
+class AreaAllowedPermissionsCreator extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,9 @@ class PermissionsTableCreator extends Migration
      */
     public function up()
     {
-        Schema::create('permissions', function (Blueprint $table) {
+        Schema::create('area_allowed_permissions', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer("userID");
+            $table->string("area_name");
             $table->string("permission");
             $table->timestamps();
         });
@@ -27,6 +27,6 @@ class PermissionsTableCreator extends Migration
      */
     public function down()
     {
-        Schema::drop('Permissions');
+        Schema::drop('area_allowed_permissions');
     }
 }
