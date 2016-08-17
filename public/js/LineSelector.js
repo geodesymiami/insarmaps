d1a583487bd86ceb0601dd024eee62edd26e8ebb
stackTom <rtiky23@gmail.com>
2016-08-17 17:56:08 -0400

    easier/better way of drawing lines with the api

--- public/js/LineSelector.js
+++ public/js/LineSelector.js
@@ -2,6 +2,8 @@
 function LineSelector(map) {
     var that = this;
     SquareSelector.call(this, map);
+
+    // remove event listener from base constructor calling
     document.removeEventListener("mousedown", that.mouseDown);
 
     this.setStartPoint = false;
@@ -13,84 +15,50 @@ function LineSelector(map) {
             that.current = that.mousePos(e);
             that.setStartPoint = false;
 
-            var polygonCoordinates = that.getPolygonBbox(that.start, that.current);
-            that.finish(polygonCoordinates);
+            that.finish([that.start, that.current]);
         }
-    };
-    /* courtesy: https://software.intel.com/en-us/blogs/2012/11/30/calculating-a-bearing-between-points-in-location-aware-apps */
-    this.bearingInDegrees = function(start, end) {
-        var degToRad = Math.PI / 180.0;
-
-        var phi1 = start.lat * degToRad;
-        var phi2 = end.lat * degToRad;
-        var lam1 = start.lng * degToRad;
-        var lam2 = end.lng * degToRad;
-
-        return Math.atan2(Math.sin(lam2 - lam1) * Math.cos(phi2),
-            Math.cos(phi1) * Math.sin(phi2) - Math.sin(phi1) * Math.cos(phi2) * Math.cos(lam2 - lam1)
-        ) * 180 / Math.PI;
-    };
-
-    this.getPolygonBbox = function(start, end) {
-        var DISTANCE = 2; // to be made dynamic
-
-        var startPoint = that.map.map.unproject(start);
-        var endPoint = that.map.map.unproject(end);
-
-        var bearing = that.bearingInDegrees(startPoint, endPoint);
-        console.log("bearing: " + bearing);
-        console.log("sin: " + Math.sin(bearing));
-        console.log("cos: " + Math.cos(bearing));
-        var otherStartLong = startPoint.lng - (DISTANCE * Math.cos(bearing));
-        var otherStartLat = startPoint.lat - (DISTANCE * Math.sin(bearing));
-        var otherEndLong = endPoint.lng - (DISTANCE * Math.cos(bearing));
-        var otherEndLat = endPoint.lat - (DISTANCE * Math.sin(bearing));
-        var polygonCoordinates = [
-            [
-                [startPoint.lng, startPoint.lat],
-                [endPoint.lng, endPoint.lat],
-                [otherEndLong, otherEndLat],
-                [otherStartLong, otherStartLat]
-            ]
-        ]; // no idea why 3D array, just see their api
-        console.log(startPoint.lat + ", " + startPoint.lng);
-        console.log(otherStartLat + ", " + otherStartLong);
-        console.log("----------------");
-        console.log(endPoint.lat + ", " + endPoint.lng);
-        console.log(otherEndLat + ", " + otherEndLong);
-
-        return polygonCoordinates;
-    };
+    };      
 
     this.finish = function(bbox) {
-    	if (that.map.map.getSource("topographyLine")) {
-    		that.map.map.removeLayer("topographyLine");
-    		that.map.map.removeSource("topographyLine");
-    	}
-        that.map.map.addSource('topographyLine', {
-            'type': 'geojson',
-            'data': {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Polygon',
-                    'coordinates': bbox
-                }
+        if (that.map.map.getSource("topographyLine")) {
+            that.map.map.removeLayer("topographyLine");
+            that.map.map.removeSource("topographyLine");
+        }
+
+        var line = new mapboxgl.GeoJSONSource();
+        var startPoint = that.map.map.unproject(bbox[0]);
+        var endPoint = that.map.map.unproject(bbox[1]);
+
+        line.setData({
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [startPoint.lng, startPoint.lat],
+                    [endPoint.lng, endPoint.lat]
+                ]
             }
         });
 
+        that.map.map.addSource('topographyLine', line);
+
         that.map.map.addLayer({
-            'id': 'topographyLine',
-            'type': 'fill',
-            'source': 'topographyLine',
-            'layout': {},
-            'paint': {
-                'fill-color': '#088',
-                'fill-opacity': 0.8
+            "id": "topographyLine",
+            "type": "line",
+            "source": "topographyLine",
+            "layout": {
+                "line-join": "round",
+                "line-cap": "round"
+            },
+            "paint": {
+                "line-color": "#000",
+                "line-width": 8
             }
         });
     };
 
-    document.addEventListener("mousedown", that.mouseDown);
+    document.addEventListener("mousedown", that.mouseDown);    
 }
 
 LineSelector.prototype = new SquareSelector(myMap);
