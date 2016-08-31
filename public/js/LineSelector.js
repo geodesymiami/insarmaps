a534ade39eb462caa83ff78f1f909b55eaa3d885
stackTom <rtiky23@gmail.com>
2016-08-31 00:30:55 -0400

    use promises to download elevation in parallel, but append them sequentially

--- public/js/LineSelector.js
+++ public/js/LineSelector.js
@@ -70,11 +70,75 @@ function LineSelector(map) {
 
         return featuresInPolygon;
     };
+    this.get512Points = function(points) {
+        return new Promise(function(resolve, reject) {
+            var elevationGetter = new google.maps.ElevationService;
+
+            elevationGetter.getElevationForLocations({
+                "locations": points.d
+            }, function(results, status) {
+                if (status === google.maps.ElevationStatus.OK) {
+                    resolve(points.i);
+                } else {
+                    console.log(status);
+                    reject(points.i);
+                }
+            });
+        });
+    };
+
+    this.getTopographyFromGoogle = function(points) {
+        var MAX_API_REQUESTS = 512;
+        var selectedPoints = [];
+        var selectedPointsChunks = [];
+        var elevations = [];
+        var curChunk = 0;
+
+        // let's split the points into chunks of 512
+        for (var i = 0; i < points.length; i++) {
+            var long = points[i].geometry.coordinates[0];
+            var lat = points[i].geometry.coordinates[1];
+
+            selectedPoints.push({
+                lat: lat,
+                lng: long
+            });
+
+            // 512 points added? append these points as a new chunk
+            if (((i + 1) % MAX_API_REQUESTS) == 0 && i != 0) {
+                selectedPointsChunks.push({ i: curChunk, d: selectedPoints });
+                selectedPoints = [];
+                curChunk++;
+            }
+        }
+        // not even multiple? add last chunk
+        if (points.length % MAX_API_REQUESTS != 0) {
+            selectedPointsChunks.push({ i: curChunk, d: selectedPoints });
+        }
+        console.log("length is " + selectedPointsChunks.length);
+
+        var chunkPromises = selectedPointsChunks.map(that.get512Points);
+        var sequence = Promise.resolve();
+
+        chunkPromises.forEach(function(promise) {
+            sequence = sequence.then(function() {
+                return promise;
+            }).then(function(currentPoints) {
+                elevations.push(currentPoints);
+                console.log("good " + currentPoints);
+            }).catch(function(err) {
+                console.log("error in promise");
+                console.log(err);
+            });
+        });
+
+        return elevations;
+    };
 
     this.finish = function(bbox) {
         if (that.map.map.getSource("topographyLine")) {
             that.map.map.removeLayer("topographyLine");
-            that.map.map.removeSource("topographyLine");            
+            that.map.map.removeSource("topographyLine");
         }
 
         if (that.map.map.getSource("test")) {
@@ -104,36 +168,7 @@ function LineSelector(map) {
             }
         });
         var selectedFeatures = that.getPointsInPolygon(that.polygonVertices[0]);
-        var test = new mapboxgl.GeoJSONSource();
-        var features = [];
-
-        for (var i = 0; i < selectedFeatures.length; i++) {
-            var long = selectedFeatures[i].geometry.coordinates[0];
-            var lat = selectedFeatures[i].geometry.coordinates[1];
-
-            features.push({
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [long, lat]
-                }                
-            });
-        }       
-
-        test.setData({
-            "type": "FeatureCollection",
-            "features": features
-        });
-        that.map.map.addSource("test", test);
-
-        that.map.map.addLayer({
-            "id": "test",
-            "type": "circle",
-            "source": "test",
-            "paint": {
-                "circle-color": "black"
-            }
-        });
+        var topography = that.getTopographyFromGoogle(selectedFeatures);
     };
 
     document.addEventListener("mousedown", that.mouseDown);
