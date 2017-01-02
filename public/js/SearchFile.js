abf439da3dbd59f64b82b0dbfcab3b6ad233dfa6
vagrant <salsazishi@gmail.com>
2017-01-02 06:05:39 +0000

    SearchFile.js now accepts searchs with not all parameters specified

--- public/js/SearchFile.js
+++ public/js/SearchFile.js
@@ -3,23 +3,36 @@ function SearchFile(container) {
 
 	// return dictionary containing unavco attribute names and values converted from user input
 	this.getSearchAttributes = function() {
-	  var options = {};
 
-	  // Input name -> Unavco name:
-	  // input-satellite -> mission, input-relative-orbit -> relative_orbit, input-first-frame -> first_frame, 
-	  // input-mode -> beam_mode, flight-direction -> flight_direction
+		var attributes = {};	// dictionary of all attributes
+		var searchAttributes = {};	// dictionary of attributes specificied by user to search datasets by
+		var numSearchAttributes = 0;	// counter to track how many attributes user specified in search
 
-	  // * NOTE: If we needs to search for more attributes, add them here and make matching buttons in map.blade.php
-	  options["mission"] = $("#input-satellite").val();
-	  options["relative_orbit"] = $("#input-relative-orbit").val();
-	  options["first_frame"] = $("#input-first-frame").val();
-	  options["beam_mode"] = $("#input-mode").val();
-	  options["flight_direction"] = $("#input-flight-direction").val();
+	  	// Input name -> Unavco name:
+	  	// input-satellite -> mission, input-relative-orbit -> relative_orbit, input-first-frame -> first_frame, 
+	  	// input-mode -> beam_mode, flight-direction -> flight_direction
+
+	  	// * NOTE: If we need to search for more attributes, add them here and make matching buttons in map.blade.php
+	  	attributes["mission"] = $("#input-satellite").val();
+	  	attributes["relative_orbit"] = $("#input-relative-orbit").val();
+	  	attributes["first_frame"] = $("#input-first-frame").val();
+	  	attributes["beam_mode"] = $("#input-mode").val();
+	  	attributes["flight_direction"] = $("#input-flight-direction").val();
 	  
-	  // TODO: Check for null values - if null, remove attribute (key, value) pair from dictionary
-	  // only search by non-null attributes
-	  // special case is if entire dictonary is empty (contains null values) - in that case return no areas
-	  return options;
+	  	// check attributes dictionary for non-null values and add them to searchAttributes dictionary
+	  	for (var key in attributes) {
+	  		if (attributes[key].length != 0) {
+	  			numSearchAttributes++;
+	  			searchAttributes[key] = attributes[key]
+	  		}
+	 	}
+
+		// return null if user inputs no attributes to search by and we get empty dictionary
+	 	if (numSearchAttributes == 0) {
+	 		return null;
+	 	}
+
+	  	return searchAttributes;
 	}
 }
 
@@ -30,17 +43,21 @@ $(window).load(function() {
 	// given a set of user inputted attribute names and values, 
 	// return an array containing areas with all attributes matching user input
 	$("#enter-button-search-attributes").click(function() {
-		console.log("clicked enter-button-seach-attributes");
-		var searchAttributes = searcher.getSearchAttributes();
+		// array of areas with attributes matching those specified by user input
+		var matchingAreas = [];
 
-		// get list of areas on map
+		// special case if user inputs no attributes to search by, we get null instead of a dictionary
+		var searchAttributes = searcher.getSearchAttributes();
+		if (searchAttributes == null) {
+			alert("Please enter parameters before searching for a dataset");
+			return;
+		} 
+
+		// get array of all areas on map
 		var attributesController = new AreaAttributesController(myMap, myMap.areaFeatures[0]);
 		var areas = myMap.areaFeatures;
 		var fileAttributes = null;
 		var attributesMatch = null;
-
-		// list of areas with attributes matching those specified by user input
-		var matchingAreas = [];
 
 		// for each area, get attributes
 		for (var i = 0; i < areas.length; i++) {
@@ -56,13 +73,21 @@ $(window).load(function() {
 					break;
 				}
 			} 
-
 			// if all attributes match, add area to array matchingAreas
 			if (attributesMatch) {
 				matchingAreas.push(areas[i]);
 			}
 		}
-		// console.log(matchingAreas);
+
+		// DOUBLE CHECK that search returned datasets with correct attributes
+		/*
+		for (var i = 0; i < matchingAreas.length; i++) {
+			console.log(matchingAreas[i].properties.attributekeys);
+			console.log(matchingAreas[i].properties.attributevalues);
+		}
+		console.log(matchingAreas.length);
+		*/
+
 		return matchingAreas;
 	});
 
