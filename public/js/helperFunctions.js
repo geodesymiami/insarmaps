5770c6f0a5b012279fc29a777517e21470b9ff7a
stackTom <rtiky23@gmail.com>
2019-01-17 19:05:25 +0000

    attempt at fixing std dev again

--- public/js/helperFunctions.js
+++ public/js/helperFunctions.js
@@ -1,18 +1,27 @@
 const MILLISECONDS_PER_YEAR = 365 * 24 * 60 * 60 * 1000;
 // take an array of displacement values and return velocity standard deviation
 var getStandardDeviation = function(displacements, slope, y, decimal_dates) {
+    console.log("slope: " + slope);
+    console.log("y: " + y);
+    console.log("displacements: " + displacements);
+    console.log("decimal_dates: " + decimal_dates);
     var v_std = 0.0;
-    t_mean = 0;
+    var t_mean = 0;
     decimal_dates.forEach(function(date) {
         t_mean += date;
     });
     t_mean /= decimal_dates.length;
 
+    console.log("y: " + y);
+    var sum_range_change = 0.0;
+    var sum_times = 0.0;
     for (i = 0; i < displacements.length; i++) {
-        dis_pred = slope * (decimal_dates[i] - decimal_dates[0]) + y
-        v_std += Math.pow(Math.abs(dis_pred - displacements[i]), 2) / Math.pow(Math.abs(decimal_dates[i] - t_mean), 2);
+        var dis_pred = slope * (decimal_dates[i] - decimal_dates[0]) + y
+        /*v_std += Math.pow(Math.abs(dis_pred - displacements[i]), 2) / Math.pow(Math.abs(decimal_dates[i] - t_mean), 2);*/
+        sum_range_change += Math.pow(Math.abs(dis_pred - displacements[i]), 2);
+        sum_times += Math.pow(Math.abs(decimal_dates[i] - t_mean), 2);
     }
-    return Math.sqrt(v_std / (displacements.length - 2));
+    return Math.sqrt(sum_range_change / (sum_times * (displacements.length - 2)));
 }
 
 // falk's date string is in format yyyymmdd - ex: 20090817 
