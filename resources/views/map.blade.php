07df4bcfe7a93aa11ee95f3ab388af718245d79c
stackTom <rtiky23@gmail.com>
2022-07-31 01:24:24 +0000

    upload mapbox gl js to 2.9.2
    get Uncaught TypeError: Cannot read properties of undefined (reading 'used')
    but everything seems to work. might be mapbox bug

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -587,8 +587,8 @@
     var urlOptions = {!! json_encode($urlOptions) !!};
     </script>
     <link rel="stylesheet" href="/css/slideout.css" />
-    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.2/mapbox-gl.css' rel='stylesheet' />
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.2/mapbox-gl.css' rel='stylesheet' />
     <script src='https://npmcdn.com/@turf/turf/turf.min.js'></script>
     <script src="https://unpkg.com/terraformer@1.0.8"></script>
     <script src="https://unpkg.com/terraformer-wkt-parser@1.1.2"></script>
