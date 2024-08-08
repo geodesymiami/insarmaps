0eb309f09c516f8416d1af324b42952c712cc098
stackTom <rtiky23@gmail.com>
2024-08-08 12:26:29 -0400

    map ports to host machine

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -v ./docker/data:/var/lib/postgresql/16/main  insarmaps_img:latest
+sudo docker run -p 80:80 -p 5432:5432 -p 8888:8888 -it -v ./docker/data:/var/lib/postgresql/16/main  insarmaps_img:latest
