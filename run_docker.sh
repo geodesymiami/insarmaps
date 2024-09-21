5aec550f4743ec272513db71686450d209f93e7c
stackTom <rtiky23@gmail.com>
2024-09-21 14:28:48 -0400

    allow user to specify custom postgresql and mbtiles directories
    not just a parent directory in which the postgresql and mbtiles directories
    have to mandatorily be under

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,6 @@
-data_dir=${1:-./docker/data}
-server_ip=${2:-localhost}
-sudo docker run -e server_ip=${server_ip} -p 80:80 -p 5432:5432 -p 8888:8888 -it -v $data_dir/postgresql:/var/lib/postgresql/16/main -v $data_dir/mbtiles:/mbtiles_dir insarmaps_img:latest
+default_data_dir=./docker/data
+data_dir=${1:-$default_data_dir/postgresql}
+mbtiles_dir=${2:-$default_data_dir/mbtiles}
+server_ip=${3:-localhost}
+sudo docker run -e server_ip=${server_ip} -p 80:80 -p 5432:5432 -p 8888:8888 -it -v $data_dir:/var/lib/postgresql/16/main -v $mbtiles_dir:/mbtiles_dir insarmaps_img:latest
 
